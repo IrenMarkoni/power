f23a9491b21b62686d11490b7033c4637b1c4d5f
Markoni2023 <markovaucilka88@gmail.com>
2023-12-13 17:48:21 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// * последовательное появление блоков при скроле
 function onEntry(entry) {
   entry.forEach((change) => {
     if (change.isIntersecting) {
@@ -11,7 +12,7 @@ let elements = document.querySelectorAll(".introduction__item");
 for (let elm of elements) {
   observer.observe(elm);
 }
-// * разворачивает блоки на странице вопрос-ответ
+// * аккордеон
 $(".options__item-header").on("click", function (e) {
   e.stopPropagation();
   if (!$(this).hasClass("active")) {
@@ -24,21 +25,34 @@ $(".options__item-header").on("click", function (e) {
   $(this).parent().find(".options__item-dropdown").slideToggle(300);
   $(this).parent().toggleClass("active");
 });
-const letters = split(animatable);
-setTimeout(() => letters.forEach((span) => (span.style.opacity = 1)), 0);
 
-function split(element) {
-  element.style.wordBreak = "break-word";
+// * анимация печатной машинки
+// const letters = split(animatable);
+// setTimeout(() => letters.forEach((span) => (span.style.opacity = 1)), 0);
 
-  const letters = element.innerText.split("").map((letter, i) => {
-    const span = document.createElement("span");
-    span.innerHTML = letter !== " " ? letter : "&nbsp;";
-    span.style.opacity = 0;
-    span.style.transitionDelay = 0.05 * i + "s";
-    return span;
-  });
+// function split(element) {
+//   element.style.wordBreak = "break-word";
 
-  element.innerHTML = "";
-  element.append(...letters);
-  return letters;
-}
+//   const letters = element.innerText.split("").map((letter, i) => {
+//     const span = document.createElement("span");
+//     span.innerHTML = letter !== " " ? letter : "&nbsp;";
+//     span.style.opacity = 0;
+//     span.style.transitionDelay = 0.05 * i + "s";
+//     return span;
+//   });
+
+//   element.innerHTML = "";
+//   element.append(...letters);
+//   return letters;
+// }
+
+// * плавный скролл на якоря
+
+$("body").on("click", '[href*="#"]', function (e) {
+  var fixed_offset = 100;
+  if (!$(this.hash).length) return;
+  $("html,body")
+    .stop()
+    .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
